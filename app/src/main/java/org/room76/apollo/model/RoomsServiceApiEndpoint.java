8f1d80e9d6761acf652feb0b73423d00599533d5
Andrii Utvenko <a.utvenko@outlook.com>
2018-02-04 12:38:10 +0200

    Add unit cafe case

--- app/src/main/java/org/room76/apollo/model/RoomsServiceApiEndpoint.java
+++ app/src/main/java/org/room76/apollo/model/RoomsServiceApiEndpoint.java
@@ -15,7 +15,7 @@ public final class RoomsServiceApiEndpoint {
     static {
         DATA = new ArrayMap<>(3);
         addRoom(new FirebaseUserMock("Queen", "https://www.hellomagazine.com/imagenes/royalty/2017102443429/the-queen-horse-earnings-revealed/0-221-286/the-queen-state-opening-crown-t.jpg"), "Oh yes!", "I demand trial by Unit testing", true, null);
-        addRoom(new FirebaseUserMock("Alabama", "https://peopledotcom.files.wordpress.com/2017/07/jennifer-aniston-fragrance-2.jpg"), "Image Room", "Room with image for testing", true, "http://www.fonstola.ru/large/201604/230397.jpg");
+        addRoom(new FirebaseUserMock("Alabama", "https://peopledotcom.files.wordpress.com/2017/07/jennifer-aniston-fragrance-2.jpg"), "Unit Cafe", "Сервіс замовлення їжі онлайн в Unit.Cafe, найкращому кафе інноваційного містечка Unit City.", true, "https://images.adsttc.com/media/images/594b/94d4/b22e/3898/a700/0492/large_jpg/10.jpg?1498125515");
         addRoom(new FirebaseUserMock("Gandalf", "https://coubsecure-s.akamaihd.net/get/b180/p/coub/simple/cw_image/07837940028/8e1dc6c1ed147af871c8e/med_1501967527_00032.jpg"), "Espresso", "UI Testing for Android", false, null);
     }
 
@@ -24,11 +24,16 @@ public final class RoomsServiceApiEndpoint {
     private static void addRoom(FirebaseUser author, String title, String description, boolean isOpen, String imageUrl) {
         Room newRoom = new Room(author, title, description, isOpen, imageUrl);
         List<Track> tracks = new ArrayList<>();
-        tracks.add(new Track("Demons", "Imagine Dragons", 200000, "http://test"));
+        tracks.add(new Track("Demons", "Imagine", 200000, "http://test"));
         tracks.add(new Track("Rape me", "Nirvana", 400000, "http://test"));
         tracks.add(new Track("Выхода нет", "Сплин", 50000, "http://test"));
         List<FirebaseUser> users = new ArrayList<>();
         users.add(author);
+
+        users.add(new FirebaseUserMock("Alba", "http://v1.popcornnews.ru/upload/jsOT7y.jpg"));
+        users.add(new FirebaseUserMock("Alba", "http://lichnosti.net/photos/4393/main.jpg"));
+        users.add(new FirebaseUserMock("Hulk", "https://vokrug.tv/pic/person/4/c/3/8/4c385380340d56acd318cf77d7777924.jpeg"));
+
         newRoom.setTracks(tracks);
         newRoom.setUsers(users);
         DATA.put(newRoom.getId(), newRoom);
